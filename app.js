992e9e83a04a91ac2d5dcf01e904691cd137e054
nimit1428 <mnimitsavant@outlook.com>
2020-06-15 17:21:43 +0530

    testing

--- app.js
+++ app.js
@@ -5,17 +5,16 @@ const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public", "css")));
+app.use(express.static(path.join(__dirname, "public", "js")));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
 
 const homeRouter = require("./routes/homerouter.routes");
 const taskRouter = require("./routes/taskie.routes");
 
-app.use(express.static(path.join(__dirname, "public", "css")));
-app.use(express.static(path.join(__dirname, "public", "js")));
-app.use(express.static(path.join(__dirname, "views")));
-
-app.use(homeRouter);
+app.get(homeRouter);
 app.use(taskRouter);
 console.log("it has started!");
 
--- routes/taskie.routes.js
+++ routes/taskie.routes.js
@@ -1,9 +1,8 @@
 const arrPaths = [];
-arrPaths[0] = "/views/task_0.html";
-arrPaths[1] = "/views/task_1.html";
+arrPaths[0] = "/views/task_1.html";
 arrPaths[1] = "/views/task_2.html";
-arrPaths[1] = "/views/task_3.html";
-arrPaths[1] = "/views/task_5.html";
+arrPaths[2] = "/views/task_3.html";
+arrPaths[3] = "/views/task_4.html";
 
 const path = require("path");
 const rootDir = require("../utils/path");
@@ -12,6 +11,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/random", (req, res) => {
+  console.log("fuck me");
   var chosenPath = arrPaths[Math.floor(Math.random() * arrPaths.length)];
   console.log(chosenPath);
   res.sendFile(path.join(rootDir + chosenPath));
